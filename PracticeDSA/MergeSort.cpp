c0994e0d5ba7ddeb0df731dc3de435a419e66b8c
Aayyush Bansal <ayush23592@gmail.com>
2024-03-22 14:38:56 +0530

    Commit for quick sort

--- PracticeDSA/MergeSort.cpp
+++ PracticeDSA/MergeSort.cpp
@@ -55,3 +55,21 @@ vector<int> MergeSort(vector<int>& arr, int startIndex, int endIndex)
 
 	return mergedArray;
 }
+
+//int main()
+//{
+//	//vector<int> arr = { -20,100,13,14,13,-36,14,100,200,-10000 };
+//	//vector<int> arr = { -20,100,-36 };
+//	//vector<int> arr = { -100};
+//
+//	// 1. Merge Sort
+//	int startIndex = 0;
+//	int endIndex = (int)(arr.size() - 1);
+//	vector<int> mergedArray = MergeSort(arr, startIndex, endIndex);
+//	cout << "\n Sorted array:- ";
+//	for (int i = 0; i < mergedArray.size(); i++)
+//	{
+//		cout << mergedArray[i] << "\t";
+//	}
+//	return 0;
+//}
